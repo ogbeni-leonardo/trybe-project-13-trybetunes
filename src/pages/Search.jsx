30cf92f3d0d0bd5500c04f6a4078d930b071fcbc
Leonardo C Araújo <ogbeni.leonardo@outlook.com>
2022-07-01 18:26:35 -0300

    Resolução do requisito 05

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -4,13 +4,53 @@ import { string } from 'prop-types';
 import Header from '../components/Header';
 
 class Search extends Component {
+  constructor() {
+    super();
+
+    this.onInputChange = this.onInputChange.bind(this);
+
+    this.state = {
+      searchInputContent: '',
+      searchButtonIsDisabled: true,
+    };
+  }
+
+  onInputChange({ target: { value } }) {
+    const hasValidValue = value.trim().length > 1;
+
+    this.setState({
+      searchInputContent: value,
+      searchButtonIsDisabled: !hasValidValue,
+    });
+  }
+
   render() {
+    const { searchInputContent, searchButtonIsDisabled } = this.state;
     const { username } = this.props;
 
     return (
       <div data-testid="page-search">
         <Header username={ username } />
-        Search
+        <form>
+          <label htmlFor="search">
+            <input
+              data-testid="search-artist-input"
+              id="search"
+              onChange={ this.onInputChange }
+              placeholder="Digite o nome do álbum ou artista..."
+              type="text"
+              value={ searchInputContent }
+            />
+          </label>
+
+          <button
+            data-testid="search-artist-button"
+            disabled={ searchButtonIsDisabled }
+            type="submit"
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
