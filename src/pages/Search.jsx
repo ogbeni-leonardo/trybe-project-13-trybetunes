fbaa7edf7637f79bb3f8d6768185d8341046e0bb
Leonardo C Araújo <ogbeni.leonardo@outlook.com>
2022-07-01 19:23:23 -0300

    Resolução do requisito 06

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,17 +1,23 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { string } from 'prop-types';
 
 import Header from '../components/Header';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
 
 class Search extends Component {
   constructor() {
     super();
 
     this.onInputChange = this.onInputChange.bind(this);
+    this.onButtonClick = this.onButtonClick.bind(this);
 
     this.state = {
       searchInputContent: '',
       searchButtonIsDisabled: true,
+      albums: [],
+      lastSearch: '',
+      loading: false,
     };
   }
 
@@ -24,8 +30,32 @@ class Search extends Component {
     });
   }
 
+  onButtonClick(event) {
+    event.preventDefault();
+
+    const { searchInputContent } = this.state;
+
+    this.setState(
+      { loading: true },
+      () => searchAlbumsAPI(searchInputContent)
+        .then((data) => this.setState({
+          albums: data,
+          lastSearch: searchInputContent,
+          searchInputContent: '',
+          loading: false,
+        })),
+    );
+  }
+
   render() {
-    const { searchInputContent, searchButtonIsDisabled } = this.state;
+    const {
+      searchInputContent,
+      searchButtonIsDisabled,
+      albums,
+      lastSearch,
+      loading,
+    } = this.state;
+
     const { username } = this.props;
 
     return (
@@ -46,11 +76,41 @@ class Search extends Component {
           <button
             data-testid="search-artist-button"
             disabled={ searchButtonIsDisabled }
+            onClick={ this.onButtonClick }
             type="submit"
           >
             Pesquisar
           </button>
         </form>
+
+        <div>
+          { lastSearch !== ''
+            && albums.length === 0
+            && <p>Nenhum álbum foi encontrado</p> }
+
+          { loading && <p>Carregando...</p> }
+
+          { !loading && albums.length > 0 && (
+            <p>
+              Resultado de álbuns de:
+              {' '}
+              { lastSearch }
+            </p>
+          ) }
+
+          <u>
+            { albums.map(({ collectionName, collectionId }) => (
+              <li key={ collectionId }>
+                <Link
+                  data-testid={ `link-to-album-${collectionId}` }
+                  to={ `/album/${collectionId}` }
+                >
+                  { collectionName }
+                </Link>
+              </li>
+            )) }
+          </u>
+        </div>
       </div>
     );
   }
