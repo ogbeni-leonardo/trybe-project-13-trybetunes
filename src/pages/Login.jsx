c86152ba5618dabe157c6a87b07e5462a6cc3953
Leonardo C Araújo <ogbeni.leonardo@outlook.com>
2022-07-01 23:00:14 -0300

    Adicionando estilo à página de Login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,6 +3,8 @@ import { string, func, shape } from 'prop-types';
 
 import { createUser } from '../services/userAPI';
 
+import styles from '../css/Login.module.css';
+
 class Login extends Component {
   constructor() {
     super();
@@ -49,27 +51,38 @@ class Login extends Component {
         { loading
           ? (<p>Carregando...</p>)
           : (
-            <form data-testid="page-login">
-              <label htmlFor="name">
-                Nome:
-                <input
-                  data-testid="login-name-input"
-                  name="username"
-                  onChange={ this.onInputChange }
-                  placeholder="Digite seu nome..."
-                  type="text"
-                  value={ username }
-                />
-              </label>
+            <form data-testid="page-login" className={ styles.formLogin }>
+              <div className={ styles.titleLoginContainer }>
+                <h1 className={ styles.titleLogin }>
+                  Trybe
+                  <span>Tunes</span>
+                </h1>
+              </div>
 
-              <button
-                data-testid="login-submit-button"
-                onClick={ this.onButtonClick }
-                type="submit"
-                disabled={ submitIsDisabled }
-              >
-                Entrar
-              </button>
+              <div className={ styles.userLoginContainer }>
+                <label htmlFor="name" className={ styles.userLogin }>
+                  Nome:
+                  <input
+                    className={ styles.inputLogin }
+                    data-testid="login-name-input"
+                    name="username"
+                    onChange={ this.onInputChange }
+                    placeholder="Digite seu nome..."
+                    type="text"
+                    value={ username }
+                  />
+                </label>
+
+                <button
+                  className={ styles.buttonLogin }
+                  data-testid="login-submit-button"
+                  onClick={ this.onButtonClick }
+                  type="submit"
+                  disabled={ submitIsDisabled }
+                >
+                  Entrar
+                </button>
+              </div>
             </form>
           )}
       </div>
