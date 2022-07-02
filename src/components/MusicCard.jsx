0802d0ebb82174683b0bbc95239151d0979f0919
Leonardo C Araújo <ogbeni.leonardo@outlook.com>
2022-07-01 21:28:09 -0300

    Resolução do requisito 08

--- src/components/MusicCard.jsx
+++ src/components/MusicCard.jsx
@@ -1,12 +1,36 @@
 import React, { Component } from 'react';
-import { shape } from 'prop-types';
+import { shape, func, bool } from 'prop-types';
 
 class MusicCard extends Component {
+  constructor(props) {
+    super(props);
+
+    this.onCheckChange = this.onCheckChange.bind(this);
+
+    const { favoriteSong } = this.props;
+
+    this.state = {
+      favoriteSong,
+    };
+  }
+
+  onCheckChange({ target: checked }, track) {
+    const { addFavoriteSong } = this.props;
+
+    this.setState({ favoriteSong: checked });
+
+    if (checked) {
+      addFavoriteSong(track);
+    }
+  }
+
   render() {
-    const { track: { trackName, previewUrl } } = this.props;
+    const { favoriteSong } = this.state;
+    const { track } = this.props;
+    const { trackId, trackName, previewUrl } = track;
 
     return (
-      <>
+      <div>
         <p>{ trackName }</p>
         <audio
           controls
@@ -16,16 +40,27 @@ class MusicCard extends Component {
           <track kind="captions" />
           O seu navegador não suporta o elemento
           {' '}
-          { ' ' }
           <code>audio</code>
         </audio>
-      </>
+        <label htmlFor={ trackId }>
+          <input
+            checked={ favoriteSong }
+            data-testid={ `checkbox-music-${trackId}` }
+            id={ trackId }
+            onChange={ (event) => this.onCheckChange(event, track) }
+            type="checkbox"
+          />
+          Favorita
+        </label>
+      </div>
     );
   }
 }
 
 MusicCard.propTypes = {
+  favoriteSong: bool.isRequired,
   track: shape({}).isRequired,
+  addFavoriteSong: func.isRequired,
 };
 
 export default MusicCard;
--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -3,33 +3,58 @@ import { string, shape } from 'prop-types';
 
 import Header from '../components/Header';
 import MusicCard from '../components/MusicCard';
+
 import getMusics from '../services/musicsAPI';
+import { addSong, getFavoriteSongs } from '../services/favoriteSongsAPI';
 
 class Album extends Component {
   constructor() {
     super();
 
+    this.addFavoriteSong = this.addFavoriteSong.bind(this);
+    this.itsFavoriteSong = this.itsFavoriteSong.bind(this);
+
     this.state = {
       albumData: [],
+      favoriteSongs: [],
+      loading: false,
     };
   }
 
   async componentDidMount() {
     const { match: { params: { id } } } = this.props;
 
-    const data = await getMusics(id);
-    this.setState({ albumData: data });
+    const albumData = await getMusics(id);
+    this.setState({ albumData });
+  }
+
+  addFavoriteSong(track) {
+    this.setState(
+      { loading: true },
+      () => {
+        addSong(track).then(() => {
+          getFavoriteSongs().then((allFavoriteSongs) => {
+            this.setState({ favoriteSongs: allFavoriteSongs, loading: false });
+          });
+        });
+      },
+    );
+  }
+
+  itsFavoriteSong(track) {
+    const { favoriteSongs } = this.state;
+    return favoriteSongs.some((favorite) => favorite.trackId === track.trackId);
   }
 
   render() {
-    const { albumData } = this.state;
+    const { albumData, loading } = this.state;
     const { username } = this.props;
 
     return (
       <div data-testid="page-album">
         <Header username={ username } />
 
-        { albumData.length > 0 && (
+        { albumData.length > 0 && !loading ? (
           <>
             <p data-testid="artist-name">
               Nome do artista:
@@ -42,12 +67,17 @@ class Album extends Component {
               { albumData[0].collectionName }
             </p>
           </>
-        ) }
+        ) : <p>Carregando...</p> }
 
-        { albumData
+        { !loading && albumData
           .filter((_music, index) => index > 0)
           .map((music) => (
-            <MusicCard key={ music.trackId } track={ music } />
+            <MusicCard
+              addFavoriteSong={ this.addFavoriteSong }
+              favoriteSong={ this.itsFavoriteSong(music) }
+              key={ music.trackId }
+              track={ music }
+            />
           ))}
       </div>
     );
